d13cd6d732fb2bcba5b0c52b43dcc0226e581fc6
boifot <boiric16@student.westerdals.no>
2018-03-05 12:13:16 +0100

    Created GameManager Singleton

---
+++ SimpleSoccer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private bool _isKickOff;
+	private GameManager _instance;
+	[SerializeField] Transform ball;
+	public GameManager Instance
+	{
+		get { return _instance;  }
+	}
+
+	void Awake()
+	{
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		_isKickOff = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Goal(int team)
+	{
+	
+	}
+}
