5ff9177e884503891f56247a0659fc3d9d98300b
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-03-05 12:22:35 +0100

    .vs ignored and removed

---
+++ SimpleSoccer/Assets/Scripts/DefencePlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefencePlayer : Player {
+
+	[SerializeField] float defaultOffenciveScalar = -15;
+	[SerializeField] float defaultRightScalar = -10;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SimpleSoccer/Assets/Scripts/HumanoidMotor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class HumanoidMotor : MonoBehaviour {
+
+	[SerializeField] float moveForce = 60;
+	[SerializeField] float stoppingRadius = 1;
+
+	Vector3 _target_point;
+	List<Vector3> _movements;
+	Rigidbody _rb;
+
+	void Start () {
+		_rb = GetComponent<Rigidbody>();
+		_target_point = transform.position;
+		_movements = new List<Vector3>();
+	}
+
+	void FixedUpdate () {
+
+		Movement();
+
+		Rotate();
+
+	}
+
+	public void MoveToPoint(Vector3 point) {
+		_target_point = point;
+	}
+
+	public void AddMovement(Vector3 movement) {
+		_movements.Add(movement);
+	}
+
+	private void Movement() {
+
+		Vector3 move = Vector3.zero;
+
+		foreach (Vector3 m in _movements) {
+			move += m;
+		}
+
+		Vector3 dir_to_point = _target_point - transform.position;
+		move = move.magnitude < stoppingRadius ? Vector3.zero : move.normalized; 
+
+		_rb.AddForce(move * moveForce * Time.deltaTime, ForceMode.Force);
+
+		_movements.Clear();
+		
+	}
+
+	private void Rotate () {
+
+		transform.rotation = Quaternion.LookRotation(_rb.velocity == Vector3.zero ? transform.forward : _rb.velocity, Vector3.up);
+
+	}
+
+}
