abd11d9de4bbe52dfcdc428ae5a9df1e93852994
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-03-05 12:02:46 +0100

    base classes

---
+++ SimpleSoccer/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	Transform teamBaseTransform;
+
+	public void SetBasePos(Transform t) {
+		teamBaseTransform = t;
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ SimpleSoccer/Assets/Scripts/Team.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour {
+
+	[SerializeField] Player[] _players;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Player p in _players) {
+			p.SetBasePos(transform);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
