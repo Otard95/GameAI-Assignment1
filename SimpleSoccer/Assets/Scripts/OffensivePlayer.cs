e0fc76a9bd335fc69f5cf73e5b4445e75feceb43
boifot <boiric16@student.westerdals.no>
2018-03-08 17:26:10 +0100

    Added states and temporary switch-case to OffensivePlayer class

--- SimpleSoccer/Assets/Scripts/OffensivePlayer.cs
+++ SimpleSoccer/Assets/Scripts/OffensivePlayer.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum States
+{
+	idle,
+	chaseBall,
+	drible,
+	recieveBall,
+	support,
+	returnHome,
+	kickBall
+};
+
 public class OffensivePlayer : Player {
 
 	[SerializeField] float defaultOffenciveScalar = -5;
@@ -9,8 +20,13 @@ public class OffensivePlayer : Player {
 
 	HumanoidMotor _motor;
 
+	States _state;
+	bool _teamGotBall;
+	bool _gotBall;
+
 	void Start () {
 		_motor = GetComponent<HumanoidMotor>();
+		_state = States.idle;
 	}
 
 	// Update is called once per frame
@@ -19,5 +35,76 @@ public class OffensivePlayer : Player {
 		Vector3 defaultPos = teamBaseTransform.position + (teamBaseTransform.forward * defaultOffenciveScalar) + (teamBaseTransform.right * defaultRightScalar);
 		_motor.MoveToPoint(defaultPos);
 
+		switch(_state)
+		{
+			case States.idle:
+			{
+				if(_teamGotBall)
+				{
+					_state = States.support;
+				}
+				else
+				{
+					_state = States.chaseBall;
+				}
+				break;
+			}
+			case States.chaseBall:
+			{
+				if(_teamGotBall)
+				{
+					_state = States.support;
+				}
+				break;
+			} 
+			case States.drible:
+			{
+				if(!_gotBall)
+				{
+					_state = States.chaseBall;
+				}
+				else if ()
+				{
+					_state = States.kickBall;
+				}
+				break;
+			} 
+			case States.kickBall:
+			{
+				if(!_gotBall)
+				{
+					if(_teamGotBall)
+					{
+						_state = States.support;
+					}
+				}
+				break;
+			} 
+			case States.recieveBall:
+			{
+				if(_gotBall)
+				{
+					_state = States.drible;
+				}
+				break;
+			}
+			case States.returnHome:
+			{
+
+				break;
+			}
+			case States.support:
+			{
+				if(!_teamGotBall)
+				{
+					_state = States.chaseBall;
+				}
+				else if (_gotBall)
+				{
+					_state = States.drible;
+				}
+				break;
+			} 
+		}
 	}
 }
