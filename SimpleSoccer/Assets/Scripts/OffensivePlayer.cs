1748cb1f4f8e45ec397bb3002de420436d09c746
boifot <boiric16@student.westerdals.no>
2018-03-09 12:27:38 +0100

    Added States and switch-cases for state transition and action

--- SimpleSoccer/Assets/Scripts/OffensivePlayer.cs
+++ SimpleSoccer/Assets/Scripts/OffensivePlayer.cs
@@ -2,20 +2,155 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum States
+{
+	idle,
+	chaseBall,
+	drible,
+	recieveBall,
+	support,
+	returnHome,
+	kickBall
+};
+
 public class OffensivePlayer : Player {
 
 	[SerializeField] float defaultOffenciveScalar = -5;
 	[SerializeField] float defaultRightScalar = -10;
 
-	new void Start () {
-		base.Start();
+	HumanoidMotor _motor;
+
+	States _state;
+
+	//TEMP
+	bool _teamGotBall;
+	bool _gotBall;
+	bool _inPosition;
+	//ENDTEMP
+
+	void Start () {
+		_motor = GetComponent<HumanoidMotor>();
 	}
 
 	// Update is called once per frame
-	void Update () {
+	public void Update () {
 
 		Vector3 defaultPos = teamBaseTransform.position + (teamBaseTransform.forward * defaultOffenciveScalar) + (teamBaseTransform.right * defaultRightScalar);
 		_motor.MoveToPoint(defaultPos);
 
+		_state = States.idle;
+
+		//State transition
+		switch(_state)
+		{
+			case States.idle:
+			{
+				if(_teamGotBall)
+				{
+					_state = States.support;
+				}
+				else
+				{
+					_state = States.chaseBall;
+				}
+				break;
+			}
+			case States.chaseBall:
+			{
+				if(_teamGotBall)
+				{
+					_state = States.support;
+				}
+				break;
+			} 
+			case States.drible:
+			{
+				if(!_gotBall)
+				{
+					_state = States.chaseBall;
+				}
+				else if (_inPosition)
+				{
+					_state = States.kickBall;
+				}
+				break;
+			} 
+			case States.kickBall:
+			{
+				if(!_gotBall)
+				{
+					if(_teamGotBall)
+					{
+						_state = States.support;
+					}
+				}
+				break;
+			} 
+			case States.recieveBall:
+			{
+				if(_gotBall)
+				{
+					_state = States.drible;
+				}
+				break;
+			}
+			case States.returnHome:
+			{
+
+				break;
+			}
+			case States.support:
+			{
+				if(!_teamGotBall)
+				{
+					_state = States.chaseBall;
+				}
+				else if (_gotBall)
+				{
+					_state = States.drible;
+				}
+				break;
+			} 
+		}
+
+		//State action
+		switch (_state)
+		{
+			case States.idle:
+			{
+				
+				break;
+			}
+			case States.chaseBall:
+			{
+				
+				break;
+			} 
+			case States.drible:
+			{
+				
+				break;
+			} 
+			case States.kickBall:
+			{
+				
+				break;
+			} 
+			case States.recieveBall:
+			{
+				
+				break;
+			}
+			case States.returnHome:
+			{
+
+				break;
+			}
+			case States.support:
+			{
+				
+				break;
+			} 
+		}
 	}
 }
